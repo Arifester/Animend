dcca43147537edc78aea3ba182d61c663c2adb25
Arifester <trainersterarif@gmail.com>
2026-01-23 23:53:23 +0700

    feat: add removeFromWishlist route to enable deletion of items from user wishlist

--- server/src/routes/animeRoutes.js
+++ server/src/routes/animeRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { addToWishlist, getWishlist } = require('../controllers/animeController');
 const verifyToken = require('../middleware/authMiddleware');
+const { addToWishlist, getWishlist, removeFromWishlist } = require('../controllers/animeController');
 
 // Protected Routes: User must be logged in
 router.post('/wishlist', verifyToken, addToWishlist);
 router.get('/wishlist', verifyToken, getWishlist);
+router.delete('/wishlist/:animeId', verifyToken, removeFromWishlist);
 
 module.exports = router;
